c9b05be656773b4f3687267cd432a5114db2b2c4
nico <nicolas.francq98@gmail.com>
2020-11-05 14:15:00 +0100

    modification mot de passe sur editer profil

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -34,10 +34,16 @@ class ProfileType extends AbstractType
                     ])
                 ],
             ])
+            ->add('oldPassword', PasswordType::class, array(
+                'label' => 'Ancien mot de passe',
+                'mapped' => false,
+                'required' => false,
+            ))
             ->add('pass', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
-                'required' => true,
+                'mapped' => false,
+                'required' => false,
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Répéter mot de passe'],
             ])
